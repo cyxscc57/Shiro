6c5c267f036156b692660b96001e1297391278d3
chenyixin <729232539@qq.com>
2020-05-27 22:47:32 +0800

    多个reamls

--- shiro-2/src/com/atguigu/shiro/realms/SecondRealm.java
+++ shiro-2/src/com/atguigu/shiro/realms/SecondRealm.java
@@ -3,21 +3,20 @@ package com.atguigu.shiro.realms;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.crypto.hash.SimpleHash;
 import org.apache.shiro.realm.AuthenticatingRealm;
-import org.apache.shiro.realm.Realm;
 import org.apache.shiro.util.ByteSource;
 
-public class ShiroRealm extends AuthenticatingRealm {
+public class SecondRealm extends AuthenticatingRealm {
 
 
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
-        System.out.println("[FirstRealm]--------");
+        System.out.println("[SecondRealm]--------");
         //1、将AuthenticationTokenq强转为UsernamePasswordToken
         UsernamePasswordToken usernamePasswordToken= (UsernamePasswordToken) authenticationToken;
         //2、从UsernamePasswordToken获取用户名
             String username=usernamePasswordToken.getUsername();
         //3、从数据库获取用户对应的记录
-        System.out.println("从数据获取的用户名"+username);
+        System.out.println("从数据库获取的用户名"+username);
         //4、用户若不存在抛出UnknownAccountException异常
         if("unknown".equals(username)){
             throw new UnknownAccountException("用户名不存在");
@@ -28,9 +27,9 @@ public class ShiroRealm extends AuthenticatingRealm {
         }
         Object credentials="";
         if("admin".equals(username)){
-            credentials="13215500ae4ea87b410201e85837af7f";
+            credentials="1b257f95ac34ef075aeb3c7b6c00f841a10268f2";
         }else if ("user".equals(username)){
-            credentials="169f8cc66024d24306331acfe1064381";
+            credentials="c0ea2ff604cab89d32943d91e2510ce09b176f30";
         }
         //6、根据用户情况构建AuthenticationInfo对象
         /*
@@ -42,14 +41,14 @@ public class ShiroRealm extends AuthenticatingRealm {
         ByteSource salt = ByteSource.Util.bytes(username);
         Object principal=username;
         String realmName=getName();
-        SimpleAuthenticationInfo simpleAuthenticationInfo=new SimpleAuthenticationInfo(principal,credentials,salt,realmName);
+        SimpleAuthenticationInfo simpleAuthenticationInfo=new SimpleAuthenticationInfo("secondRealm",credentials,salt,realmName);
         return simpleAuthenticationInfo;
     }
 
     public static void main(String[] args) {
-        String hashAlgorithmName="MD5";
+        String hashAlgorithmName="SHA1";
         Object credentials="12345";
-        ByteSource salt = ByteSource.Util.bytes("user");
+        ByteSource salt = ByteSource.Util.bytes("admin");
         Object result=new SimpleHash(hashAlgorithmName,credentials,salt,1024);
         System.out.println(result);
     }
